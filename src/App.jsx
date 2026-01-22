2b6fa4c8058459e34aa2b8a5ff5a70b4965f85f7
Sunghoon Kang <sunghoonkang@Mac.cable.rcn.com>
2026-01-22 15:10:14 -0500

    Fixed lvl timer

--- src/App.jsx
+++ src/App.jsx
@@ -134,13 +134,16 @@ function App() {
         ) : (
           <ContentCard title={"10min EXP"} content={0} />
         )}
-        {timeLeftToLevel ? (
+        {timeLeftToLevel && expGained > 0 ? (
           <ContentCard
             title={"Time Left For a level"}
             content={`${hourToLevel >= 100 ? hourToLevel : twoDigits(hourToLevel)}:${twoDigits(minuteToLevel)}:${twoDigits(secondToLevel)}`}
           />
         ) : (
-          <ContentCard title={"Time Left For a level"} content={`00:00:00`} />
+          <ContentCard
+            title={"Time Left For a level"}
+            content={`Not Measured`}
+          />
         )}
         {/*   const totalExpToLevel = (Number(firstExp) / Number(expPercent)) * 100; */}
       </div>
